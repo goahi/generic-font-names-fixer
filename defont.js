d711fa5941d409f63261135fa9a7fedb76536ad6
goahi <goai@protonmail.com>
2022-07-23 01:49:36 +0900

    fix: error handling

--- defont.js
+++ defont.js
@@ -1,23 +1,27 @@
-//replace style sheets
 const sheets = document.styleSheets;
 
+//replace style sheets
 for (let i = 0; i < sheets.length; i++) {
     //StyleSheetList is an array-like object, but it can't be iterated
-    const sheet = sheets[i].cssRules;
+    try {
+        const sheet = sheets[i].cssRules;
 
-    for (let j = 0; j < sheet.length; j++) {
-        const style = sheet[j];
-        let font_family = style.style.fontFamily;
-        if (font_family) {
-            const replaced = font_family.replace(/"(sans-serif|serif|sans|monospace)"/gi, "$1");
-            style.style.fontFamily = replaced;
+        for (let j = 0; j < sheet.length; j++) {
+            const style = sheet[j];
+            if (!style.style) {
+                continue;
+            }
+            const font_family = style.style.fontFamily;
+            if (font_family) {
+                const replaced = font_family.replace(/"(sans-serif|serif|sans|monospace)"/gi, "$1");
+                style.style.fontFamily = replaced;
+            }
         }
-    }
+    } catch (e) {}
 }
 
-//replace inline style sheets
 const inlineSheets = document.querySelectorAll("style");
-
+//replace styles in <style> tags
 inlineSheets.forEach((sheet) => {
     const css = sheet.textContent;
     sheet.textContent = css.replace(/"(sans-serif|serif|sans|monospace)"/gi, "$1");
